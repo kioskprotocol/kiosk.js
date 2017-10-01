488cb30ee989df45788df25828e0e9c4fa5ac64b
richmcateer <rmcateer1@gmail.com>
2017-09-30 22:43:39 -0400

    Owner works

--- test/test.js
+++ test/test.js
@@ -1,53 +1,59 @@
 const kiosk = require("../index.js");
-// const registryABI = require("./registry.js")
 const testrpc = require("ethereumjs-testrpc");
 const Web3 = require("web3");
 const assert = require("assert");
 const async = require("async");
 const fs = require("fs");
-const solc = require("solc");
-const Promise = require("bluebird");
-const chai = require("chai"),
+var solc = require("solc");
+var Promise = require("bluebird");
+var chai = require("chai"),
     expect = chai.expect,
     should = chai.should();
 
-describe("kiosk", function() {
-    this.timeout(15000);
+describe("Kiosk", function() {
+    var web3;
+    var kioskClient;
+    var alice;
+    var genesisDIN = 1000000000;
 
-    let web3;
-    let registry;
-    const genesisDIN = 1000000000;
-
-    before(async function() {
-        const provider = testrpc.provider();
+    before(function(done) {
+        this.timeout(20000);
         web3 = new Web3();
-        web3.setProvider(provider);
-        const getAccountsAsync = Promise.promisify(web3.eth.getAccounts);
-        const accounts = await getAccountsAsync();
+        web3.setProvider(testrpc.provider());
 
-        const alice = accounts[0];
-        const source = fs.readFileSync("test/DINRegistry.sol").toString();
-        const compiled = solc.compile(source, 1);
-        const deployer = compiled.contracts[":DINRegistry"];
-        const registryContract = await web3.eth.contract(
-            JSON.parse(deployer.interface)
-        );
-        const result = await registryContract.new(
-            genesisDIN,
-            {
-                from: alice,
-                data: deployer.bytecode,
-                gas: 4700000
-            },
-            (err, result) => {
-                console.log(result);
-                done();
-            }
-        );
-        console.log(result)
+        web3.eth.getAccounts(function(err, accounts) {
+            alice = accounts[0];
+            var source = fs.readFileSync("test/DINRegistry.sol").toString();
+            var compiled = solc.compile(source, 1);
+            var deployer = compiled.contracts[":DINRegistry"];
+            var registryContract = web3.eth.contract(
+                JSON.parse(deployer.interface)
+            );
+
+            // Deploy the contract
+            registryContract.new(
+                genesisDIN,
+                {
+                    from: accounts[0],
+                    data: deployer.bytecode,
+                    gas: 4700000
+                },
+                function(err, registry) {
+                    if (registry.address != undefined) {
+                        kioskClient = new kiosk(web3, registry);
+                        done();
+                    }
+                }
+            );
+        });
     });
 
-    it("should test", () => {
-        console.log("YES!");
+    describe('#owner()', function() {
+        it("should return the correct owner of a DIN", function(done) {
+            kioskClient.owner(1000000000).then(function(result) {
+                assert.equal(result, alice);
+            }).catch(assert.isError).finally(done);
+        });
     });
+
 });
