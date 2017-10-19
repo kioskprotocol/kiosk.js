0c724be3acea45a7490b3f4cd2afe5c99c87e58b
richmcateer <rmcateer1@gmail.com>
2017-10-19 14:04:32 -0400

    Everything works except sending raw transaction

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var Web3 = require("web3");
-var Contracts = require("./contracts.js");
-var contracts = new Contracts();
+var contracts = new (require("./contracts.js"))();
 var Promise = require("bluebird");
+// var Account = require("eth-lib/lib/account");
 // var CryptoJS = require("crypto-js");
 // var Transaction = require("ethereumjs-tx");
 // var ABI = require("ethereumjs-abi");
@@ -11,10 +11,14 @@ class Kiosk {
     constructor(web3) {
         this.web3 = web3;
 
-        this.registry = new web3.eth
-            .Contract(contracts.registryABI, contracts.registryAddressKovan);
-        this.buy = new web3.eth
-            .Contract(contracts.buyABI, contracts.buyAddressKovan);
+        this.registry = new web3.eth.Contract(
+            contracts.registryABI,
+            contracts.registryAddressKovan
+        );
+        this.buy = new web3.eth.Contract(
+            contracts.buyABI,
+            contracts.buyAddressKovan
+        );
     }
 
     owner(DIN) {
@@ -27,21 +31,64 @@ class Kiosk {
 
     productURL(DIN) {
         return this.resolver(DIN).then(resolverAddr => {
-            const resolver = new this.web3.eth.Contract(contracts.resolverABI, resolverAddr);
+            const resolver = new this.web3.eth.Contract(
+                contracts.resolverABI,
+                resolverAddr
+            );
             return resolver.methods.productURL(DIN).call();
         });
     }
 
-    signPriceMessage(DIN, price, priceValidUntil, account) {
-        const args = [DIN, price, priceValidUntil];
-        const argTypes = ["uint256", "uint256", "uint256"];
-        const msg = ABI.soliditySHA3(argTypes, args);
-        return this.web3.eth.sign(account, util.bufferToHex(msg)).then(result => {
-            let signature = util.fromRpcSig(result);
-            const { v, r, s } = signature;
-            signature.r = util.bufferToHex(r);
-            signature.s = util.bufferToHex(s);
-            return signature;
+    isValidSignature(signer, hash, v, r, s) {
+        return this.buy.methods.isValidSignature(signer, hash, v, r, s).call();
+    }
+
+    signPriceMessage(DIN, price, priceValidUntil, privateKey) {
+        const hash = this.web3.utils.soliditySha3(
+            { type: "uint256", value: DIN },
+            { type: "uint256", value: price },
+            { type: "uint256", value: priceValidUntil }
+        );
+        const signature = this.web3.eth.accounts.signHash(hash, privateKey);
+        return signature;
+    }
+
+    signBuyTransaction(
+        DIN,
+        quantity,
+        totalValue,
+        priceValidUntil,
+        v,
+        r,
+        s,
+        account,
+        privateKey
+    ) {
+        const data = this.web3.eth.abi.encodeParameters(
+            [
+                "uint256",
+                "uint256",
+                "uint256",
+                "uint256",
+                "uint8",
+                "bytes32",
+                "bytes32"
+            ],
+            [DIN, quantity, totalValue, priceValidUntil, v, r, s]
+        );
+        return this.web3.eth.getTransactionCount(account).then(nonce => {
+            const transaction = {
+                nonce: nonce,
+                to: this.buy.address,
+                data: data,
+                value: 0,
+                gas: 4700000
+            };
+            const signedTx = this.web3.eth.accounts.signTransaction(
+                transaction,
+                privateKey
+            );
+            return signedTx;
         });
     }
 }
--- test/kovan.js
+++ test/kovan.js
@@ -9,56 +9,86 @@ require("dotenv").config();
 describe("kovan", () => {
     let web3;
     let kiosk;
+    let buyer;
+    let seller; // Owner of DIN 1000000001 on Kovan
 
     // Product
     const DIN = 1000000001;
+    const quantity = 1;
+    const price = 8000000;
+    const priceValidUntil = 1514160000;
+
+    // Signature
+    let signature;
+    let rawTx;
 
     before(async () => {
-        web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA_KOVAN));
-        web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY);
+        web3 = new Web3(
+            new Web3.providers.HttpProvider(process.env.INFURA_KOVAN)
+        );
+        web3.eth.accounts.wallet.add(process.env.BUYER_PRIVATE_KEY);
+        web3.eth.accounts.wallet.add(process.env.SELLER_PRIVATE_KEY);
+        buyer = web3.eth.accounts.wallet[0];
+        seller = web3.eth.accounts.wallet[1];
         kiosk = new Kiosk(web3);
     });
 
-    it("should return the correct owner of a DIN", () => {
-        return kiosk.owner(DIN).then(owner => {
-            console.log(owner);
-        });
+    it("should return the correct owner of a DIN", async () => {
+        const owner = await kiosk.owner(DIN);
+        expect(owner).to.equal(seller.address);
     });
 
     it("should return the correct resolver of a DIN", async () => {
         const resolver = await kiosk.resolver(DIN);
-        console.log(resolver);
-        // expect(resolverAddr).to.equal(resolver.address);
+        expect(resolver).to.equal("0xA9b81c7d571717f0817688252EF2C9cCc039B939");
     });
 
     it("should get the product URL for a given DIN", async () => {
         const url = await kiosk.productURL(DIN);
-        console.log(url);
-        // expect(url).to.equal("https://www.google.com/");
+        expect(url).to.equal("https://kiosk-demo-shop.herokuapp.com/v0/products/");
     });
 
     it("should sign a price message", async () => {
-        const DIN = 1000000001;
-        const price = 8000000;
-        const priceValidUntil = 1514160000;
-        const signature = await kiosk.signPriceMessage(DIN, price, priceValidUntil, account);
+        signature = await kiosk.signPriceMessage(
+            DIN,
+            price,
+            priceValidUntil,
+            seller.privateKey
+        );
         console.log(signature);
-    })
-
-    it("should validate a signature", async () => {
-        // const signer = "0xabad46016a62b94c98febe31176835df3d0ac83c";
-        // const DIN = 1000000001;
-        // const price = 8000000;
-        // const priceValidUntil = 1514160000;
-        // const msg = ABI.soliditySHA3(DIN, price, priceValidUntil);
-        // const hash = util.bufferToHex(msg);
-        // const signature = {
-        //     v: 27,
-        //     r: "0x003be33a198f20a5c5efbe94e3908684d4c1d40d3661687bd96ac176857bfbac",
-        //     s: "0x117ecb27373e797743837b38d58cbc1ef1c19312c88ad3e6f12b96f90361ba77" 
-        // }
-        // const valid = await kiosk.isValidSignature(signer, hash, signature.v, signature.r, signature.s);
-        // console.log(valid);
     });
 
+    it("should validate a signature", async () => {
+        const hash = web3.utils.soliditySha3(
+            { type: "uint256", value: DIN },
+            { type: "uint256", value: price },
+            { type: "uint256", value: priceValidUntil }
+        );
+        const valid = await kiosk.isValidSignature(
+            seller.address,
+            hash,
+            signature.v,
+            signature.r,
+            signature.s
+        );
+        expect(valid).to.equal(true);
+    });
+
+    it("should sign a buy transaction", async () => {
+        const signedTx = await kiosk.signBuyTransaction(
+            DIN,
+            quantity,
+            price,
+            priceValidUntil,
+            signature.v,
+            signature.r,
+            signature.s,
+            buyer.address,
+            buyer.privateKey
+        );
+        rawTx = signedTx.rawTransaction;
+        console.log(rawTx);
+        // const result = await web3.eth.sendSignedTransaction(rawTx);
+        // console.log("RESULT: " + result);
+    });
 });
