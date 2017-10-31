3ec47ba69eb6a00d4c2dd0c3b98814fb361e3bbb
richmcateer <rmcateer1@gmail.com>
2017-10-31 14:36:12 -0400

    Split into two modules

--- src/index.js
+++ src/index.js
@@ -104,36 +104,6 @@ class Kiosk {
         });
     }
 
-    signPriceMessage(
-        DIN,
-        price,
-        priceCurrency,
-        priceValidUntil,
-        affiliateFee,
-        privateKey
-    ) {
-        // REQUIRES WEB3 BETA!
-        const hash = this.web3.utils.soliditySha3(
-            { type: "uint256", value: DIN },
-            { type: "uint256", value: price },
-            { type: "address", value: priceCurrency },
-            { type: "uint256", value: priceValidUntil },
-            { type: "uint256", value: affiliateFee }
-        );
-        var prefix = "\x19Ethereum Signed Message:\n32";
-        var messageHash = utils.soliditySha3(prefix, hash);
-        var signature = Account.sign(messageHash, privateKey);
-        var vrs = Account.decodeSignature(signature);
-        var v = vrs[0];
-        return {
-            messageHash: messageHash,
-            v: this.web3.utils.toDecimal(v),
-            r: vrs[1],
-            s: vrs[2],
-            signature: signature
-        };
-    }
-
     // getOrder(orderID) {
     //     return this.checkout
     //         .getPastEvents("NewOrder", {
@@ -152,100 +122,6 @@ class Kiosk {
     //         .call();
     // }
 
-    // signAddToCartTransaction(DIN, account, privateKey) {
-    //     const inputs = [{ type: "uint256", name: "DIN" }];
-    //     const args = [DIN];
-    //     return this.signTransaction(
-    //         "addToCart",
-    //         inputs,
-    //         args,
-    //         this.cart._address,
-    //         0,
-    //         account,
-    //         privateKey
-    //     );
-    // }
-
-    // signBuyTransaction(
-    //     DIN,
-    //     quantity,
-    //     totalPrice,
-    //     priceCurrency,
-    //     priceValidUntil,
-    //     affiliateFee,
-    //     affiliate,
-    //     v,
-    //     r,
-    //     s,
-    //     account,
-    //     privateKey
-    // ) {
-    //     const inputs = [
-    //         { type: "uint256[5]", name: "orderValues" },
-    //         { type: "address[2]", name: "orderAddresses" },
-    //         { type: "uint8", name: "v" },
-    //         { type: "bytes32", name: "r" },
-    //         { type: "bytes32", name: "s" }
-    //     ];
-    //     const orderValues = [
-    //         DIN,
-    //         quantity,
-    //         totalPrice,
-    //         priceValidUntil,
-    //         affiliateFee
-    //     ];
-    //     const orderAddresses = [priceCurrency, affiliate];
-    //     const args = [orderValues, orderAddresses, v, r, s];
-
-    //     let value = 0;
-
-    //     if (priceCurrency === "0x0000000000000000000000000000000000000000") {
-    //         // If paying in Ether, we need to set the value
-    //         value = totalPrice;
-    //     }
-
-    //     return this.signTransaction(
-    //         "buy",
-    //         inputs,
-    //         args,
-    //         this.checkout._address,
-    //         value,
-    //         account,
-    //         privateKey
-    //     );
-    // }
-
-    // signTransaction(
-    //     functionName,
-    //     inputs,
-    //     args,
-    //     contractAddr,
-    //     value,
-    //     account,
-    //     privateKey
-    // ) {
-    //     const data = this.web3.eth.abi.encodeFunctionCall(
-    //         {
-    //             name: functionName,
-    //             type: "function",
-    //             inputs: inputs
-    //         },
-    //         args
-    //     );
-    //     const transaction = {
-    //         from: account,
-    //         to: contractAddr,
-    //         data: data,
-    //         value: value,
-    //         gas: "200000",
-    //         gasPrice: "20000000000"
-    //     };
-    //     return this.web3.eth.accounts
-    //         .signTransaction(transaction, privateKey)
-    //         .then(signedTx => {
-    //             return signedTx.rawTransaction;
-    //         });
-    // }
 }
 
 module.exports = Kiosk;
