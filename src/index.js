5adde0fd5e2c8f3334267e11d84abf56abc32659
richmcateer <rmcateer1@gmail.com>
2018-01-30 16:38:36 -0500

    Stash

--- src/index.js
+++ src/index.js
@@ -1,61 +1,61 @@
 var DINRegistryContract = require("../contracts/build/contracts/DINRegistry.json");
-var ResolverContract = require("../contracts/build/contracts/StandardResolver.json");
-var CheckoutContract = require("../contracts/build/contracts/Checkout.json");
-var OrdersContract = require("../contracts/build/contracts/Orders.json");
-var ERC20Contract = require("../contracts/build/contracts/ERC20.json");
-var LoyaltyTokenRegistryContract = require("../contracts/build/contracts/LoyaltyTokenRegistry.json");
-var MarketTokenContract = require("../contracts/build/contracts/MarketToken.json");
 var Account = require("eth-lib/lib/account");
+const noAccount = "0x0000000000000000000000000000000000000000";
 
 class Kiosk {
     constructor(web3, networkId) {
-        // Rinkeby, Kovan, Ganache (local)
-        const supportedNetworkIds = ["4", "42", "5777"];
-
-        if (supportedNetworkIds.includes(networkId.toString()) === false) {
-            return null;
-        }
-
         this.web3 = web3;
 
         // Initialize contracts
         var registryAddress =
-            DINRegistryContract["networks"][networkId]["address"];
+            DINRegistryContract["networks"]["4447"]["address"];
         this.registry = new this.web3.eth.Contract(
             DINRegistryContract.abi,
             registryAddress
         );
-        var checkoutAddress =
-            CheckoutContract["networks"][networkId]["address"];
-        this.checkout = new this.web3.eth.Contract(
-            CheckoutContract.abi,
-            checkoutAddress
-        );
-        var ordersAddress = OrdersContract["networks"][networkId]["address"];
-        this.orders = new this.web3.eth.Contract(
-            OrdersContract.abi,
-            ordersAddress
-        );
-        var loyaltyRegistryAddress =
-            LoyaltyTokenRegistryContract["networks"][networkId]["address"];
-        this.loyaltyRegistry = new this.web3.eth.Contract(
-            LoyaltyTokenRegistryContract.abi,
-            loyaltyRegistryAddress
-        );
-        var marketTokenAddress =
-            MarketTokenContract["networks"][networkId]["address"];
-        this.marketToken = new this.web3.eth.Contract(
-            MarketTokenContract.abi,
-            marketTokenAddress
-        );
     }
 
+    registerDIN(account) {
+        return new Promise((resolve, reject) => {
+            this.registry.methods
+                .selfRegisterDIN()
+                .send({
+                    from: account
+                })
+                .then(result => {
+                    resolve(result);
+                });
+        });
+    }
+
+    setResolver() {}
+
     owner(DIN) {
-        return this.registry.methods.owner(DIN).call();
+        return new Promise((resolve, reject) => {
+            this.registry.methods
+                .owner(DIN)
+                .call()
+                .then(result => {
+                    resolve(result);
+                })
+                .catch(err => {
+                    resolve(noAccount);
+                });
+        });
     }
 
     resolver(DIN) {
-        return this.registry.methods.resolver(DIN).call();
+        return new Promise((resolve, reject) => {
+            this.registry.methods
+                .resolver(DIN)
+                .call()
+                .then(result => {
+                    resolve(result);
+                })
+                .catch(err => {
+                    resolve(noAccount);
+                });
+        });
     }
 
     productURL(DIN) {
@@ -68,240 +68,224 @@ class Kiosk {
         });
     }
 
-    merchant(DIN) {
-        return this.resolver(DIN).then(resolverAddr => {
-            const resolver = new this.web3.eth.Contract(
-                ResolverContract.abi,
-                resolverAddr
-            );
-            return resolver.methods.merchant(DIN).call();
-        });
-    }
+    // merchant(DIN) {
+    //     return this.resolver(DIN).then(resolverAddr => {
+    //         const resolver = new this.web3.eth.Contract(
+    //             ResolverContract.abi,
+    //             resolverAddr
+    //         );
+    //         return resolver.methods.merchant(DIN).call();
+    //     });
+    // }
 
-    hash(nonce) {
-        return this.web3.utils.sha3(nonce);
-    }
+    // hash(nonce) {
+    //     return this.web3.utils.sha3(nonce);
+    // }
 
-    getETHBalance(account) {
-        return this.web3.eth.getBalance(account);
-    }
+    // getETHBalance(account) {
+    //     return this.web3.eth.getBalance(account);
+    // }
 
-    getMARKBalance(account) {
-        return this.marketToken.methods.balanceOf(account).call();
-    }
+    // getMARKBalance(account) {
+    //     return this.marketToken.methods.balanceOf(account).call();
+    // }
 
-    getERC20Balance(account, tokenAddress) {
-        const tokenContract = new this.web3.eth.Contract(
-            ERC20Contract.abi,
-            tokenAddress
-        );
-        return tokenContract.methods.balanceOf(account).call();
-    }
+    // getERC20Balance(account, tokenAddress) {
+    //     const tokenContract = new this.web3.eth.Contract(
+    //         ERC20Contract.abi,
+    //         tokenAddress
+    //     );
+    //     return tokenContract.methods.balanceOf(account).call();
+    // }
 
-    // Returns the last token deployed by a given merchant via the Loyalty Token Registry
-    getLoyaltyToken(merchant) {
-        return this.loyaltyRegistry
-            .getPastEvents("NewToken", {
-                filter: { merchant: merchant },
-                fromBlock: 0,
-                toBlock: "latest"
-            })
-            .then(events => {
-                if (events.length > 0) {
-                    return events[events.length - 1].returnValues.token;
-                }
-                return null;
-            });
-    }
+    // signPriceMessage(product, privateKey) {
+    //     const hash = this.web3.utils.soliditySha3(
+    //         { type: "uint256", value: product.DIN },
+    //         { type: "uint256", value: product.price },
+    //         { type: "uint256", value: product.priceValidUntil },
+    //         { type: "address", value: product.merchant },
+    //         { type: "uint256", value: product.affiliateReward },
+    //         { type: "uint256", value: product.loyaltyReward },
+    //         { type: "address", value: product.loyaltyToken }
+    //     );
+    //     var prefix = "\x19Ethereum Signed Message:\n32";
+    //     var messageHash = this.web3.utils.soliditySha3(prefix, hash);
+    //     var signature = Account.sign(messageHash, privateKey);
+    //     var vrs = Account.decodeSignature(signature);
+    //     var v = vrs[0];
+    //     return {
+    //         v: this.web3.utils.toDecimal(v),
+    //         r: vrs[1],
+    //         s: vrs[2]
+    //     };
+    // }
 
-    signPriceMessage(product, privateKey) {
-        const hash = this.web3.utils.soliditySha3(
-            { type: "uint256", value: product.DIN },
-            { type: "uint256", value: product.price },
-            { type: "uint256", value: product.priceValidUntil },
-            { type: "address", value: product.merchant },
-            { type: "uint256", value: product.affiliateReward },
-            { type: "uint256", value: product.loyaltyReward },
-            { type: "address", value: product.loyaltyToken }
-        );
-        var prefix = "\x19Ethereum Signed Message:\n32";
-        var messageHash = this.web3.utils.soliditySha3(prefix, hash);
-        var signature = Account.sign(messageHash, privateKey);
-        var vrs = Account.decodeSignature(signature);
-        var v = vrs[0];
-        return {
-            v: this.web3.utils.toDecimal(v),
-            r: vrs[1],
-            s: vrs[2]
-        };
-    }
+    // // This is the method to use
+    // executeBuy(order, loyaltyAmount, nonceHash, signature, account) {
+    //     return new Promise((resolve, reject) => {
+    //         this.buy(
+    //             order,
+    //             loyaltyAmount,
+    //             nonceHash,
+    //             signature,
+    //             account,
+    //             false
+    //         ).then(result => {
+    //             if (result > 0) {
+    //                 // Execute the actual buy transaction
+    //                 this.buy(
+    //                     order,
+    //                     loyaltyAmount,
+    //                     nonceHash,
+    //                     signature,
+    //                     account,
+    //                     true
+    //                 )
+    //                     .then(result => {
+    //                         resolve(result);
+    //                     })
+    //                     .catch(error => {
+    //                         reject(error);
+    //                     });
+    //             } else {
+    //                 reject("There was an error when calling this transaction");
+    //             }
+    //         });
+    //     });
+    // }
 
-    // This is the method to use
-    executeBuy(order, loyaltyAmount, nonceHash, signature, account) {
-        return new Promise((resolve, reject) => {
-            this.buy(
-                order,
-                loyaltyAmount,
-                nonceHash,
-                signature,
-                account,
-                false
-            ).then(result => {
-                if (result > 0) {
-                    // Execute the actual buy transaction
-                    this.buy(
-                        order,
-                        loyaltyAmount,
-                        nonceHash,
-                        signature,
-                        account,
-                        true
-                    )
-                        .then(result => {
-                            resolve(result);
-                        })
-                        .catch(error => {
-                            reject(error);
-                        });
-                } else {
-                    reject("There was an error when calling this transaction");
-                }
-            });
-        });
-    }
+    // buy(
+    //     order,
+    //     loyaltyAmount,
+    //     nonceHash,
+    //     signature,
+    //     account,
+    //     isTransaction = false
+    // ) {
+    //     return new Promise((resolve, reject) => {
+    //         const orderValues = [
+    //             order.DIN,
+    //             order.quantity,
+    //             order.totalPrice,
+    //             order.priceValidUntil,
+    //             order.affiliateReward,
+    //             order.loyaltyReward
+    //         ];
+    //         const orderAddresses = [
+    //             order.merchant,
+    //             order.affiliate,
+    //             order.loyaltyToken
+    //         ];
+    //         const value = Math.max(order.totalPrice - loyaltyAmount, 0);
+    //         const txParams = {
+    //             from: account,
+    //             value: value,
+    //             gas: 200000,
+    //             gasPrice: this.web3.utils.toWei("20", "gwei")
+    //         };
+    //         const buy = this.checkout.methods.buy(
+    //             orderValues,
+    //             orderAddresses,
+    //             nonceHash,
+    //             signature.v,
+    //             signature.r,
+    //             signature.s
+    //         );
+    //         // Dry run
+    //         if (isTransaction === false) {
+    //             buy.call(txParams, (error, result) => {
+    //                 if (error) {
+    //                     reject(error);
+    //                 } else {
+    //                     resolve(result);
+    //                 }
+    //             });
+    //         } else {
+    //             buy
+    //                 .send(txParams)
+    //                 .on("receipt", receipt => {
+    //                     resolve(receipt);
+    //                 })
+    //                 .on("error", err => {
+    //                     reject(err);
+    //                 });
+    //         }
+    //     });
+    // }
 
-    buy(
-        order,
-        loyaltyAmount,
-        nonceHash,
-        signature,
-        account,
-        isTransaction = false
-    ) {
-        return new Promise((resolve, reject) => {
-            const orderValues = [
-                order.DIN,
-                order.quantity,
-                order.totalPrice,
-                order.priceValidUntil,
-                order.affiliateReward,
-                order.loyaltyReward
-            ];
-            const orderAddresses = [
-                order.merchant,
-                order.affiliate,
-                order.loyaltyToken
-            ];
-            const value = Math.max(order.totalPrice - loyaltyAmount, 0);
-            const txParams = {
-                from: account,
-                value: value,
-                gas: 200000,
-                gasPrice: this.web3.utils.toWei("20", "gwei")
-            };
-            const buy = this.checkout.methods.buy(
-                orderValues,
-                orderAddresses,
-                nonceHash,
-                signature.v,
-                signature.r,
-                signature.s
-            );
-            // Dry run
-            if (isTransaction === false) {
-                buy.call(txParams, (error, result) => {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        resolve(result);
-                    }
-                });
-            } else {
-                buy
-                    .send(txParams)
-                    .on("receipt", receipt => {
-                        resolve(receipt);
-                    })
-                    .on("error", err => {
-                        reject(err);
-                    });
-            }
-        });
-    }
+    // isValidSignature(signer, hash, v, r, s) {
+    //     return this.checkout.methods
+    //         .isValidSignature(signer, hash, v, r, s)
+    //         .call();
+    // }
 
-    isValidSignature(signer, hash, v, r, s) {
-        return this.checkout.methods
-            .isValidSignature(signer, hash, v, r, s)
-            .call();
-    }
+    // isValidLoyaltyToken(tokenAddress) {
+    //     return this.loyaltyRegistry.methods.whitelist(tokenAddress).call();
+    // }
 
-    isValidLoyaltyToken(tokenAddress) {
-        return this.loyaltyRegistry.methods.whitelist(tokenAddress).call();
-    }
+    // getOrder(txHash) {
+    //     return new Promise((resolve, reject) => {
+    //         this.web3.eth.getTransactionReceipt(txHash).then(result => {
+    //             const data = result.logs[0].data;
+    //             const params = this.web3.eth.abi.decodeParameters(
+    //                 [
+    //                     "bytes32",
+    //                     "address",
+    //                     "uint256",
+    //                     "uint256",
+    //                     "uint256",
+    //                     "uint256"
+    //                 ],
+    //                 data
+    //             );
+    //             const topics = result.logs[0].topics;
+    //             const orderID = this.web3.eth.abi.decodeParameter(
+    //                 "uint256",
+    //                 topics[1]
+    //             );
+    //             const buyer = this.web3.eth.abi.decodeParameter(
+    //                 "address",
+    //                 topics[2]
+    //             );
+    //             const merchant = this.web3.eth.abi.decodeParameter(
+    //                 "address",
+    //                 topics[3]
+    //             );
+    //             const order = {
+    //                 orderID: orderID,
+    //                 nonceHash: params["0"],
+    //                 checkout: params["1"],
+    //                 buyer: buyer,
+    //                 merchant: merchant,
+    //                 DIN: params["2"],
+    //                 quantity: params["3"],
+    //                 totalPrice: params["4"],
+    //                 timestamp: params["5"]
+    //             };
+    //             resolve(order);
+    //         });
+    //     });
+    // }
 
-    getOrder(txHash) {
-        return new Promise((resolve, reject) => {
-            this.web3.eth.getTransactionReceipt(txHash).then(result => {
-                const data = result.logs[0].data;
-                const params = this.web3.eth.abi.decodeParameters(
-                    [
-                        "bytes32",
-                        "address",
-                        "uint256",
-                        "uint256",
-                        "uint256",
-                        "uint256"
-                    ],
-                    data
-                );
-                const topics = result.logs[0].topics;
-                const orderID = this.web3.eth.abi.decodeParameter(
-                    "uint256",
-                    topics[1]
-                );
-                const buyer = this.web3.eth.abi.decodeParameter(
-                    "address",
-                    topics[2]
-                );
-                const merchant = this.web3.eth.abi.decodeParameter(
-                    "address",
-                    topics[3]
-                );
-                const order = {
-                    orderID: orderID,
-                    nonceHash: params["0"],
-                    checkout: params["1"],
-                    buyer: buyer,
-                    merchant: merchant,
-                    DIN: params["2"],
-                    quantity: params["3"],
-                    totalPrice: params["4"],
-                    timestamp: params["5"]
-                };
-                resolve(order);
-            });
-        });
-    }
+    // isValidOrder(txHash, nonce, merchant) {
+    //     return new Promise(resolve => {
+    //         this.getOrder(txHash).then(order => {
+    //             const nonceHash = this.hash(nonce);
+    //             if (
+    //                 order.nonceHash === nonceHash &&
+    //                 order.merchant === merchant
+    //             ) {
+    //                 resolve(true);
+    //             } else {
+    //                 resolve(false);
+    //             }
+    //         });
+    //     });
+    // }
 
-    isValidOrder(txHash, nonce, merchant) {
-        return new Promise(resolve => {
-            this.getOrder(txHash).then(order => {
-                const nonceHash = this.hash(nonce);
-                if (
-                    order.nonceHash === nonceHash &&
-                    order.merchant === merchant
-                ) {
-                    resolve(true);
-                } else {
-                    resolve(false);
-                }
-            });
-        });
-    }
-
-    getOrderIndex() {
-        return this.orders.methods.orderIndex().call();
-    }
+    // getOrderIndex() {
+    //     return this.orders.methods.orderIndex().call();
+    // }
 }
 
 module.exports = Kiosk;
--- test/test.js
+++ test/test.js
@@ -7,8 +7,8 @@ var chai = require("chai"),
 require("dotenv").config();
 
 describe("test", () => {
-    let web3;
-    let kiosk;
+    let web3; // web3.js
+    let kiosk; // kiosk.js
     let buyer;
     let merchant;
     const merchantPrivateKey = "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
@@ -19,14 +19,10 @@ describe("test", () => {
     const quantity = 1;
     const price = 8000000;
     const priceValidUntil = 1514160000;
-    const affiliateReward = 0;
-    const loyaltyReward = 0;
-    const loyaltyToken = "0x0000000000000000000000000000000000000000";
 
     // Order
     let order;
     let txHash;
-    const affiliate = "0x0000000000000000000000000000000000000000";
     const nonce = "blah";
 
     // Signature
@@ -34,28 +30,14 @@ describe("test", () => {
 
     before(async () => {
         web3 = new Web3(
-            new Web3.providers.HttpProvider("http://localhost:7545") // Ganache
+            new Web3.providers.HttpProvider("http://localhost:9545") // Ganache
         );
-        kiosk = new Kiosk(web3, "5777");
-
+        kiosk = new Kiosk(web3, "4447");
         const accounts = await web3.eth.getAccounts();
         merchant = accounts[0];
         buyer = accounts[1];
 
-        product = {
-            DIN: DIN,
-            quantity: quantity,
-            price: price,
-            priceValidUntil: priceValidUntil,
-            merchant: merchant,
-            affiliateReward: affiliateReward,
-            loyaltyReward: loyaltyReward,
-            loyaltyToken: loyaltyToken,
-            totalPrice: price * quantity,
-            affiliate: affiliate
-        };
-
-        order = product;
+        await kiosk.registerDIN(merchant);
     });
 
     it("should return the correct owner of a DIN", async () => {
@@ -63,61 +45,61 @@ describe("test", () => {
         expect(owner).to.equal(merchant);
     });
 
-    it("should get the product URL for a given DIN", async () => {
-        const url = await kiosk.productURL(DIN);
-        expect(url).to.equal("https://kiosk-shopify.herokuapp.com/products/");
-    });
+    // it("should get the product URL for a given DIN", async () => {
+    //     const url = await kiosk.productURL(DIN);
+    //     expect(url).to.equal("https://kiosk-shopify.herokuapp.com/products/");
+    // });
 
-    it("should sign a price message", async () => {
-        signature = await kiosk.signPriceMessage(product, merchantPrivateKey);
-        expect(signature).to.exist;
-    });
+    // it("should sign a price message", async () => {
+    //     signature = await kiosk.signPriceMessage(product, merchantPrivateKey);
+    //     expect(signature).to.exist;
+    // });
 
-    it("should validate a signature", async () => {
-        const hash = web3.utils.soliditySha3(
-            DIN,
-            price,
-            priceValidUntil,
-            merchant,
-            affiliateReward,
-            loyaltyReward,
-            loyaltyToken
-        );
-        const valid = await kiosk.isValidSignature(
-            merchant,
-            hash,
-            signature.v,
-            signature.r,
-            signature.s
-        );
-        expect(valid).to.equal(true);
-    });
+    // it("should validate a signature", async () => {
+    //     const hash = web3.utils.soliditySha3(
+    //         DIN,
+    //         price,
+    //         priceValidUntil,
+    //         merchant,
+    //         affiliateReward,
+    //         loyaltyReward,
+    //         loyaltyToken
+    //     );
+    //     const valid = await kiosk.isValidSignature(
+    //         merchant,
+    //         hash,
+    //         signature.v,
+    //         signature.r,
+    //         signature.s
+    //     );
+    //     expect(valid).to.equal(true);
+    // });
 
-    it("should buy a product", async () => {
-        const nonceHash = kiosk.hash(nonce);
-        const result = await kiosk.executeBuy(order, 0, nonceHash, signature, buyer);
-        txHash = result.transactionHash;
-        expect(result).to.exist;
-    })
+    // it("should buy a product", async () => {
+    //     const nonceHash = kiosk.hash(nonce);
+    //     const result = await kiosk.executeBuy(order, 0, nonceHash, signature, buyer);
+    //     txHash = result.transactionHash;
+    //     expect(result).to.exist;
+    // })
 
-    it("should get order events", async () => {
-        const order = await kiosk.getOrder(txHash);
-        expect(order.DIN).to.equal(DIN.toString());
-    });
+    // it("should get order events", async () => {
+    //     const order = await kiosk.getOrder(txHash);
+    //     expect(order.DIN).to.equal(DIN.toString());
+    // });
 
-    it("should validate an order", async () => {
-        const isValid = await kiosk.isValidOrder(txHash, nonce, merchant);
-        expect(isValid).to.equal(true);
-    });
+    // it("should validate an order", async () => {
+    //     const isValid = await kiosk.isValidOrder(txHash, nonce, merchant);
+    //     expect(isValid).to.equal(true);
+    // });
 
-    it("should not validate an order with an incorrect nonce", async () => {
-        const isValid = await kiosk.isValidOrder(txHash, "fake", merchant);
-        expect(isValid).to.equal(false);
-    });
+    // it("should not validate an order with an incorrect nonce", async () => {
+    //     const isValid = await kiosk.isValidOrder(txHash, "fake", merchant);
+    //     expect(isValid).to.equal(false);
+    // });
 
-    it("should have the correct order index", async () => {
-        const index = await kiosk.getOrderIndex();
-        expect(index).to.exist;
-    });
+    // it("should have the correct order index", async () => {
+    //     const index = await kiosk.getOrderIndex();
+    //     expect(index).to.exist;
+    // });
 
 });
